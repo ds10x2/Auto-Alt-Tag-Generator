9d28db64e43d46b07f6f2e26e5ea878e5d36da75
sunwooiljoon <wnsdl1659@naver.com>
2023-05-30 20:19:34 +0900

    feat : 화면 안에 있는 사진에 대해서 api호출 후 대체

--- chrome-extension/alt-tag-generate.js
+++ chrome-extension/alt-tag-generate.js
@@ -1,12 +1,30 @@
-// 모든 img 태그 선택
-const imgTags = document.querySelectorAll("img");
-console.log(imgTags);
-// img 태그를 반복하면서 alt 속성이 없는 경우 수정
-imgTags.forEach((img) => {
-  const altText = img.getAttribute("alt");
-  if (!altText || altText.trim() === "") {
-    // alt 속성이 비어있거나 공백 문자열인 경우
-    img.setAttribute("alt", "대체 텍스트 테스트");
-    console.log("alt 속성이 비어있습니다.");
-  }
-});
+//현재 화면에 보이는 태그인지 확인하는 함수
+function isElementInViewport(el) {
+  var rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+{
+	API_URL = "http://127.0.0.1:5000/"
+	// 모든 img 태그 선택
+	const imgTags = document.querySelectorAll("img");
+	console.log(imgTags);
+	// img 태그를 반복하면서 alt 속성이 없는 경우 수정
+	imgTags.forEach((img) => {
+		isVisible = isElementInViewport(img);
+		const altText = img.getAttribute("alt");
+		if (isVisible) {
+			fetch(API_URL).then((response) => response.json())
+			.then((data) => {
+				new_alt = data['altText'];
+				img.setAttribute("alt", new_alt);
+				console.log(new_alt, "대체 텍스트 생성!");
+			})
+		}
+	});
+}
